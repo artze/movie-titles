aa451a4b40de291d431499680183fa1b2604b5ef
artze <arthurwtm@gmail.com>
2019-09-04 23:33:48 +0800

    Set up axios to fetch titles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import TitleList from './TitleList';
 
-function App() {
-  return (
-    <div>
-      <div>Movie Titles</div>
-      <TitleList />
-    </div>
-  );
+class App extends React.Component {
+  fetchMovieTitles() {
+    axios
+      .get(
+        'https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=1&perPage=20'
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
+  componentDidMount() {
+    this.fetchMovieTitles();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Movie Titles</div>
+        <TitleList />
+      </div>
+    );
+  }
 }
 
 export default App;
