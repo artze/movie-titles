ed3b4e68218606842480cb34d25929e3c41aba20
artze <arthurwtm@gmail.com>
2019-09-04 13:33:48 +0800

    Set up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': [
+      'eslint:recommended',
+      'plugin:prettier/recommended'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'prettier'
+    ],
+    'rules': {
+        'prettier/prettier': ['error', {
+          'arrowParens': 'always',
+          'bracketSpacing': true,
+          'endOfLine': 'lf',
+          'jsxBracketSameLine': false,
+          'semi': true,
+          'singleQuote': true,
+          'tabWidth': 2,
+        }],
+        'react/jsx-uses-react': 'error',
+    }
+};
