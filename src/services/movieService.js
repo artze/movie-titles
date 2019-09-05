51279ab688e6ad6bf77485daf2b46ec775a76126
artze <arthurwtm@gmail.com>
2019-09-05 23:36:34 +0800

    Append movieLists when scrolling to end of page

--- src/services/movieService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const getMovieListByPage = (pageNumber) =>
-  axios
-    .get(
-      `https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=${pageNumber}&perPage=20`
-    )
-    .then((res) => res.data.data);
