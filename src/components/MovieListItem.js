db92f888caaa90d872ac4eb5fb0a01a557b03349
artze <arthurwtm@gmail.com>
2019-09-06 18:52:45 +0800

    Style movie list item

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 
 const MovieListItem = (props) => (
   <Link to={`/movies/${props.movieRowItem.getId()}`}>
-    <img
-      src={props.movieRowItem.getPosterImageUrl()}
-      alt={props.movieRowItem.getTitle()}
-    />
-    <span>{props.movieRowItem.getTitle()}</span>
+    <div className="movie-list-item__container">
+      <img
+        src={props.movieRowItem.getPosterImageUrl()}
+        alt={props.movieRowItem.getTitle()}
+      />
+      <span className="movie-list-item__link-text">
+        {props.movieRowItem.getTitle()}
+      </span>
+    </div>
   </Link>
 );
 
