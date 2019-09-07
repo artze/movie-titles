a8804f6258a1c032a0c97af140797d34386316e6
artze <arthurwtm@gmail.com>
2019-09-07 18:52:25 +0800

    Add unit test for movieService

---
+++ src/tests/fixtures/movieLists.js
@@ -0,0 +1,38 @@
+export const movieLists = {
+  page1: {
+    data: {
+      data: [
+        {
+          row_id: 1,
+          type: 'abc'
+        },
+        {
+          row_id: 2,
+          type: 'def'
+        }
+      ],
+      pagination: {
+        page: 1,
+        totalPages: 2
+      }
+    }
+  },
+  page2: {
+    data: {
+      data: [
+        {
+          row_id: 3,
+          type: 'ghi'
+        },
+        {
+          row_id: 4,
+          type: 'jkl'
+        }
+      ],
+      pagination: {
+        page: 2,
+        totalPages: 2
+      }
+    }
+  }
+};
